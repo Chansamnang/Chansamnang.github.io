6f46bdbf1eb9b123b5b86aa918176a54ee25a517
Chansamnang <limchansamnang@gmail.com>
2019-07-15 23:20:21 +0700

    04_max.py

---
+++ Python Bootcamp/Week01/04_max.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a first number: ")
+num2 = input("Enter a second number: ")
+if num1 > num2:
+    print(num1 + " > " + num2)
+elif num2 > num1:
+    print(num2 + " > " + num1)
+else:
+    print(num1 + " = " + num2)
